78f27ea9d309bc893fd5c20b7d53364bcec2b5dd
huilight <huilight@outlook.com>
2019-12-26 16:47:43 +0800

    code with books.

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')
@@ -7,7 +10,10 @@ app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 db = SQLAlchemy(app)
+toolbar = DebugToolbarExtension(app)
 
 # 这些模块需要从构造文件中导入程序实例
 # 为避免循环依赖，将这些导入语句放在文件的末尾定义
--- sayhello/settings.py
+++ sayhello/settings.py
@@ -2,7 +2,7 @@ import os
 
 from sayhello import app
 
-dev_db = 'sql:///' + os.path.join(os.path.dirname(app.root_path), 'data.db')
+dev_db = 'sqlite:///' + os.path.join(os.path.dirname(app.root_path), 'data.db')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'aoIEMC3V4B5Q56PsASDFsdf2fVJO.WEF')
 
